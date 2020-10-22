4edf3a4d9e15ca3b126505c51a3d18ae6a9d68d9
a4johnny <40929308+a4johnny@users.noreply.github.com>
2020-10-22 21:29:27 +0800

    POISSON sum complete

--- poisson.py
+++ poisson.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import matplotlib.style as style
+import math
 from IPython.core.display import HTML
 #from scipy.stats import poisson
 
@@ -12,18 +13,57 @@ style.use('fivethirtyeight')
 
 #left == x #pmf
 #plt.bar(x=np.arange(100), height=(stats.poisson.pmf(np.arange(100), mu=20)), width=0.75, alpha=0.75)
-k = 1
-n = 10
-t = 1
+#需要量
+k = 200
+n = 100
+t = 5
 p = k/n
-mu= n * p *t
+
+count = 0
+pp = []
+for i in range(100) :
+    count = count + 0.01
+    pp.append(count)
+
+bb = []
+bb2 = []
+bbb3 = []
+tempgod = 0
+thePP = 0
+for p in pp :
+    mu= n * p * t
+    b = stats.poisson.pmf(k, mu)
+    bb.append(b)  #不同p達成相同k的機率 => bb
+    ko = math.sqrt(k)
+    temp = 0
+    num = 0
+    bbb = []
+    for kk in range(int(k - 3*ko), int(k + 3*ko), 1) : #平均值往上下找三個標準差  同p不同k
+        b = stats.poisson.pmf(kk, mu)  #計算此k下此p機率
+        temp = temp + b
+        bbb.append(b) #此p下不同k總計
+    bbb3.append(bbb) #二維陣列 紀錄所有p不同k
+    num = num + 1
+    if temp > tempgod : #找到累積最大值時的p (thePP)
+        tempgod = temp
+        thePP = p
+    bb2.append(temp) #紀錄
+
+a = plt.bar(np.arange(100), bb)
+plt.show()
+aa = plt.bar(np.arange(100), bb2)
+plt.show()
+print("thePP : ", thePP)
+
 b = stats.poisson.pmf(k, mu)
 #x = np.arange(stats.poisson.ppf(0.01, mu), stats.poisson.ppf(0.99, mu))
 c = stats.poisson.pmf(np.arange(k), mu)
-k2 = k
-plt.bar(np.arange(k2), stats.poisson.pmf(np.arange(k2), mu))
+## np.arange(k) 照k大小
+#plt.bar(np.arange(k), stats.poisson.pmf(np.arange(k), mu)) # X,Y data type 要一樣
 
 #cdf
+#累加 1 - 10
+#d = stats.poisson.cdf(10, mu)
 #plt.plot(np.arange(20), stats.poisson.cdf(np.arange(20), mu=2), color="#fc4f30")
 #plt.plot(np.arange(k), stats.poisson.cdf(np.arange(k), mu), color="#fc4f30")
 
@@ -31,8 +71,10 @@ plt.bar(np.arange(k2), stats.poisson.pmf(np.arange(k2), mu))
 #plt.text(x=8, y=0.45, s="pmf(poisson)", alpha=0.75, weight="bold", color="#008fd5")
 #plt.text(x=8.5, y=0.9, s="cdf", rotation=.75, weight="bold", color="#fc4f30")
 
-a = plt.show()
-print(a)
+plt.show()
+
+#a = plt.show()
+#print(a)
 #--------------------------------------------------------------------------------------------
 # plt.figure(dpi=100)
 # # PDF λ=1
