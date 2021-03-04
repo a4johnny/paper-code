ae98d1ce3fbe61432a8430d1280a4a7f3990c0a0
a4 <wop3345@gmail.com>
2021-03-04 19:42:23 +0800

    正式一版

--- poisson_oneTOmany2.py
+++ poisson_oneTOmany2.py
@@ -12,7 +12,7 @@ def Pcal (delp, trueN):
     print("pppk", k)
     n = trueN
     t = 5
-    #p = k/n
+    # p = k/n
 
     count = 0
     pp = []
@@ -29,20 +29,20 @@ def Pcal (delp, trueN):
     for p in pp:
         mu = n * p * t
         b = stats.poisson.pmf(k, mu)
-        bb.append(b)  #不同p達成相同k的機率 => bb
-        ko = math.sqrt(k) # poisson 標準差 > 期望值是樣本母數=變異數，標準差=變異數開根號
+        bb.append(b)  # 不同p達成相同k的機率 => bb
+        ko = math.sqrt(k)  # poisson 標準差 > 期望值是樣本母數=變異數，標準差=變異數開根號
         temp = 0
         if b > 0.005:
             bbb = []
-            for kk in range(int(k - 1*ko), int(k + 2*ko), 1) : #平均值往上下找三個標準差  同p不同k
-                b = stats.poisson.pmf(kk, mu)  #計算此k下此p機率
+            for kk in range(int(k - 1*ko), int(k + 2*ko), 1):  # 平均值往上下找三個標準差  同p不同k
+                b = stats.poisson.pmf(kk, mu)  # 計算此k下此p機率
                 temp = temp + b
-                bbb.append(b) #此p下不同k總計
-            bbb3.append(bbb) #二維陣列 紀錄所有p不同k
-            if temp > tempgod : #找到累積最大值時的p (thePP)
+                bbb.append(b)  # 此p下不同k總計
+            bbb3.append(bbb)  # 二維陣列 紀錄所有p不同k
+            if temp > tempgod:  # 找到累積最大值時的p (thePP)
                 tempgod = temp
                 thePP = p
-        bb2.append(temp) #紀錄
+        bb2.append(temp)  # 紀錄
 
     # plt.plot(np.arange(10000), bb2)
     # plt.show()
