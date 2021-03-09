8cf37a252463ed5d1f15eb8e2b06d39b332e07ec
a4 <wop3345@gmail.com>
2021-03-09 15:16:59 +0800

    test array in dict

---
+++ DictWithArray.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.style as style
+import math
+import random
+from IPython.core.display import HTML
+import poisson_oneTOmany2 as poi
+import seaborn as sn
+
+
+if __name__ == '__main__':
+    hashmap1 = {}
+    LTrange = 10
+    for i in range(LTrange):
+        stri = str(i + 1)
+        array1 = []
+        if (i + 1) == 3:
+            array1.append(1)
+            array1.append(2)
+            array1.append(3)
+        hashmap1[stri] = array1
+
+    print(hashmap1)
+    # array1 = hashmap1['3']
+    print(hashmap1['3'][1])
