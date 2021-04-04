953b64b5a3a9b127c7b8ea9d8c059302964dd31e
a4 <wop3345@gmail.com>
2021-04-04 18:25:33 +0800

    random walk simple version

--- ramdom_walk.py
+++ ramdom_walk.py
@@ -17,25 +17,30 @@ class Man:
         self.area = area
 
 
-def addman(u):
-    i = 0
-
+def addman(areacount, user):
+    addn = random.randint(200, 300)
+    for i in range(addn):
+        u = Man(random.randint(0, 8), random.randint(0, 8), 0)
+        u.area = areajudge(u)
+        areacount[u.area] += 1
+        user.append(user)
+    print("i am working")
 
 def areajudge(u):
     if u.x < 3:
         if u.y < 3:
             u.area = 1
-        elif 3 <= u.y <= 4:
+        elif 3 <= u.y <= 5:
             u.area = 4
-        elif 6 <= u.y <= 7:
+        elif 6 <= u.y <= 8:
             u.area = 7
     elif 3 <= u.x <= 5:
         if u.y < 3:
             u.area = 2
         elif 3 <= u.y <= 5:
-            u.area = 4
+            u.area = 5
         elif 6 <= u.y <= 8:
-            u.area = 7
+            u.area = 8
     elif 6 <= u.x <= 8:
         if u.y < 3:
             u.area = 3
@@ -49,12 +54,16 @@ def areajudge(u):
     return u.area
 
 
-def rw(user, n):  # 隨機亂走
-    for i in range(n):
+def rw(user, areacount):  # 隨機亂走
+    for i in range(len(user)):
         LorR = random.randint(0, 1)
         TorB = random.randint(0, 1)
         rangeX = random.randint(0, 5)
         rangeY = random.randint(0, 5)
+
+        long = len(user)
+        if i >= long:
+            break
 
         if LorR == 1:
             user[i].x += rangeX
@@ -67,23 +76,34 @@ def rw(user, n):  # 隨機亂走
             user[i].y -= rangeY
 
         user[i].area = areajudge(user[i])
+
         if user[i].area == 0:
             del user[i]
         elif user[i].area != 0:
+            areacount[user[i].area] += 1
 
-        print(user[i].area)
+    print("areacount", areacount)
+    addman(areacount, user)
 
 
 def userinit(n):  # n現在區域內總人數
     user = []
+
+    areacount = []
+    for i in range(1, 11, 1):  # 起使 結束 地增值
+        areacount.append(0)
+
     for i in range(n):
         u = Man(random.randint(0, 8), random.randint(0, 8), 0)
         u.area = areajudge(u)
+        areacount[u.area] += 1
+        if u.area == 0:
+            print(u.x, u.y)
         user.append(u)
-    rw(user, n)
+    return user, areacount
 
 
-if __name__ == '__main__':
-    userinit(3000)
-
-    i = 1
+if __name__ == '__main__':  # 此程式當主程式執行時，從這行開始
+    for i in range(1):
+        user, areacount = userinit(3000)
+        rw(user, areacount)
