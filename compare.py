abf306634863df9929fe920cf6a3beebb17763da
a4 <wop3345@gmail.com>
2021-06-30 21:47:53 +0800

    審判日前-2

--- compare.py
+++ compare.py
@@ -51,7 +51,7 @@ def init(user):
 
 
 if __name__ == '__main__':
-    needk = 280
+    needk = 1600
     coveragelist = []
     nmlist = []
     user, area = rw.userinit(9000)
@@ -69,14 +69,14 @@ if __name__ == '__main__':
             if i is 5:
                 nm += 1
 
-        coverage = nm/50
+        coverage = nm/450
         for iii in range(5):
             nmlist.append(nm)
             coveragelist.append(coverage)
         print('coverage:', coverage, 'time:', time, user[1].areal)
     x = np.arange(500)
     plt.plot(x, nmlist)
-    plt.ylim([0, 100])
+    plt.ylim([0, 500])
     plt.grid()
     plt.show()
     print('nm:', nmlist)
